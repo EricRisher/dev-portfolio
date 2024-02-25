a296f17555d1c12bf8de01fef39f550ae365388f
EricRisher <rishereric13@gmail.com>
2024-02-24 22:09:12 -0800

    fixed bugs

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ function App() {
           <Navbar />
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="*" element={<div>404 Not Found!</div>} />
           </Routes>
         </div>
       </Router>
