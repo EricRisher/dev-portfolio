8c8b585ee531f153145010867563e069da16d3b2
Eric Risher <erisher@Erics-MacBook-Air.local>
2024-04-18 16:13:35 -0700

    added more animation and photography section

--- src/Pages/Home/Photography.jsx
+++ src/Pages/Home/Photography.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
+import React from "react";
+import data from "../../data/index.json";
 
 function Photography() {
   return (
     <div>
-      <h1>Photography</h1>
-      <p>Photos will go here eventually..</p>
+      <h1 className="photo--header">Photography</h1>
+      <div className="photo--container">
+        {data?.photography?.map((photo, index) => (
+          <div key={index} className="photo--card">
+            <img src={photo.src} alt={photo.alt} />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
 
-export default Photography
+export default Photography;
